319ed3eedd8308a68bbcaa98d1889c35cb588d41
davulamadhulika <davulamadhulika@gmail.com>
2025-07-02 23:07:52 +0530

    file created and updated

---
+++ src/Components/PlaceDetails.jsx
@@ -0,0 +1,10 @@
+function PlaceDetails({ place }) {
+  return (
+    <div>
+      <h2>{place.name}</h2>
+      <p>{place.description}</p>
+      {/* Gallery and info */}
+    </div>
+  );
+}
+export default PlaceDetails;
